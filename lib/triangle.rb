1f23762fecc9b7a47d7a54d482f04569d6279730
Marlowe Recht <recht.marlowe@gmail.com>
2023-01-31 22:37:23 +0100

    commit tree 58579613cbf38dce2330aef363fc00d2da6ac987

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if self.side1 <= 0 || self.side2 <= 0 || self.side3 <= 0
+      raise TriangleError
+    elsif self.side1 + self.side2 <= self.side3 || self.side2 + self.side3 <= self.side1 || self.side1 + self.side3 <= self.side2
+      raise TriangleError
+    elsif self.side1 == self.side2 && self.side1 == self.side3 && self.side2 == self.side3
+      :equilateral
+    elsif self.side1 != self.side2 && self.side1 != self.side3 && self.side2 != self.side3
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "be better"
+    end
+  end
+
 end
